1bcc2e59b6a3700c4cd24c3debd7e724f21eb9cf
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2011-10-24 17:01:10 +0200

    Begin .stat_incs

--- lib/stat_request_parser.rb
+++ lib/stat_request_parser.rb
@@ -1,5 +1,139 @@
-require "stat_request_parser/version"
+require 'stat_request_parser/version'
+require 'useragent'
 
 module StatRequestParser
-  # Your code goes here...
+
+  GLOBAL_KEYS = [:t, :e, :h, :d]
+
+  # Returns MongoDB incs for SiteStat & VideoStat collections
+  #
+  # { site: { t: 'ovjigy83', inc: {} }, videos: [{ t: 'ovjigy83', vt: 'abcd1234', inc: {} }, {}] }
+  #
+  def self.stat_incs(params, user_agent, hits = 1)
+    incs = { videos: [] }
+    if (params.keys & GLOBAL_KEYS) == GLOBAL_KEYS
+      case params[:e]
+      when 'l'
+        bp = browser_and_platform_key(user_agent)
+
+        # Site
+        site = { inc: {} }
+        # Site token
+        site[:t] = params[:t]
+        # Site Page Visits
+        site[:inc]['pv.' + params[:h]] = hits
+        # Browser + Plateform
+        site[:inc]['bp.' + bp] = hits
+        # Player Mode + Device hash
+        params[:pm].uniq.each do |pm|
+          site[:inc]['md.' + pm + '.' + params[:d]] = params[:pm].count(pm) * hits
+        end
+        incs[:site] = site
+
+        # Videos
+        params[:vt].each_with_index do |vt, i|
+          video = { inc: {} }
+          video[:t]  = params[:t]
+          video[:vt] = vt
+          # Video load
+          video[:inc]['vl.' + params[:h]] = hits
+          # Browser + Plateform
+          video[:inc]['bp.' + bp] = hits
+          # Player Mode + Device hash
+          video[:inc]['md.' + params[:pm][i] + '.' + params[:d]] = hits
+          incs[:videos] << video
+        end
+      end
+    end
+    incs
+  end
+
+  def self.incs_from_params_and_user_agent(params, user_agent, hits)
+    incs   = {}
+    params = Addressable::URI.parse(params).query_values || {}
+    if params.key?('e') && params.key?('h')
+      case params['e']
+      when 'l' # Player load &  Video prepare
+        unless params.key?('po') # video prepare only
+          if params.key?('em') # embed
+            # Page Visits embeds
+            incs['pv.em'] = hits
+          else
+            # Page Visits
+            incs['pv.' + params['h']] = hits
+            # Browser + Plateform
+            if %w[m e].include?(params['h'])
+              incs['bp.' + browser_and_platform_key(user_agent)] = hits
+            end
+          end
+        end
+        # Player Mode + Device hash
+        if %w[m e].include?(params['h']) && params.key?('pm') && params.key?('d')
+          params['pm'].uniq.each do |pm|
+            incs['md.' + pm + '.' + params['d']] = params['pm'].count(pm) * hits
+          end
+        end
+      when 's' # Video start (play)
+        # Video Views
+        incs['vv.' + params['h']] = hits
+      end
+    end
+    incs
+  end
+
+  def self.inc_and_json(params, user_agent)
+    inc, json = {}, {}
+    case params[:e]
+    when 'l' # Player load &  Video prepare
+      unless params.key?(:po) # video prepare only
+        inc['pv.' + params[:h]] = 1 # Page Visits
+        inc['bp.' + browser_and_platform_key(user_agent)] = 1 # Browser + Plateform
+        json = { 'pv' => 1, 'bp' => { browser_and_platform_key(user_agent) => 1 } }
+      end
+      # Player Mode + Device hash
+      if params.key?(:pm) && params.key?(:d)
+        json['md'] = { 'h' => {}, 'f' => {} }
+        params[:pm].uniq.each do |pm|
+          inc['md.' + pm + '.' + params[:d]] = params[:pm].count(pm)
+          json['md'][pm] = { params[:d] => params[:pm].count(pm) }
+        end
+      end
+    when 's' # Video start (play)
+      # Video Views
+      inc['vv.' + params[:h]] = 1
+      json = { 'vv' => 1 }
+    end
+    [inc, json]
+  end
+
+
+  def self.browser_and_platform_key(user_agent)
+    useragent    = UserAgent.parse(user_agent)
+    browser_key  = SUPPORTED_BROWSER[useragent.browser] || "oth"
+    platform_key = SUPPORTED_PLATEFORM[useragent.platform] || (useragent.mobile? ? "otm" : "otd")
+    browser_key + '-' + platform_key
+  end
+  SUPPORTED_BROWSER = {
+    "Firefox"           => "fir",
+    "Chrome"            => "chr",
+    "Internet Explorer" => "iex",
+    "Safari"            => "saf",
+    "Android"           => "and",
+    "BlackBerry"        => "rim",
+    "webOS"             => "weo",
+    "Opera"             => "ope"
+  }
+  SUPPORTED_PLATEFORM = {
+    "Windows"       => "win",
+    "Macintosh"     => "osx",
+    "iPad"          => "ipa",
+    "iPhone"        => "iph",
+    "iPod"          => "ipo",
+    "Linux"         => "lin",
+    "Android"       => "and",
+    "BlackBerry"    => "rim",
+    "webOS"         => "weo",
+    "Windows Phone" => "wip"
+  }
+
 end
--- spec/stat_request_parser_spec.rb
+++ spec/stat_request_parser_spec.rb
@@ -2,6 +2,45 @@ require 'spec_helper'
 
 describe StatRequestParser do
 
-  specify { 1.should eql 1 }
+  describe ".stat_incs" do
+    let(:user_agent) { "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_1) AppleWebKit/534.48.3 (KHTML, like Gecko) Version/5.1 Safari/534.48.3" }
+
+    context "load event" do
+
+      specify { subject.stat_incs({
+          t: 'ibvjcopp', e: 'l', h: 'm', d: 'd', vt: ['abcd1234'], pm: ['h']
+        }, user_agent).should eql({
+          site: { t: 'ibvjcopp', inc: { 'pv.m' => 1, "bp.saf-osx" => 1, "md.h.d" => 1 } },
+          videos: [
+            { t: 'ibvjcopp', vt: 'abcd1234', inc: { 'vl.m' => 1, "bp.saf-osx" => 1, "md.h.d" => 1 } }
+          ]
+        })
+      }
+
+    end
+  end
+
+
+  describe ".browser_and_platform_key" do
+    specify { subject.browser_and_platform_key("Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1").should eql("saf-osx") }
+    specify { subject.browser_and_platform_key("Mozilla/5.0 (X11; U; Linux amd64; rv:5.0) Gecko/20100101 Firefox/5.0 (Debian)").should eql("fir-lin") }
+    specify { subject.browser_and_platform_key("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)").should eql("iex-win") }
+    specify { subject.browser_and_platform_key("Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.815.0 Safari/535.1").should eql("chr-win") }
+    specify { subject.browser_and_platform_key("Mozilla/5.0 (Linux; U; Android 2.3.4; fr-fr; HTC Desire Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1").should eql("and-and") }
+    specify { subject.browser_and_platform_key("Mozilla/5.0 (BlackBerry; U; BlackBerry 9700; en-US) AppleWebKit/534.8+ (KHTML, like Gecko) Version/6.0.0.546 Mobile Safari/534.8+").should eql("rim-rim") }
+    specify { subject.browser_and_platform_key("BlackBerry9700/5.0.0.862 Profile/MIDP-2.1 Configuration/CLDC-1.1 VendorID/120").should eql("rim-rim") }
+    specify { subject.browser_and_platform_key("Opera/9.80 (X11; Linux x86_64; U; Ubuntu/10.10 (maverick); pl) Presto/2.7.62 Version/11.01").should eql("ope-lin") }
+    specify { subject.browser_and_platform_key("Mozilla/5.0 (webOS/1.0; U; en-US) AppleWebKit/525.27.1 (KHTML, like Geko) Version/1.0 Safari/525.27.1 Pre/1.0").should eql("weo-weo") }
+    specify { subject.browser_and_platform_key("Mozilla/4.0 (compatible; MSIE 7.0; Windows Phone OS 7.0; Trident/3.1; IEMobile/7.0) Asus;Galaxy6").should eql("iex-wip") }
+    specify { subject.browser_and_platform_key("Lynx/2.8.7rel.2 libwww-FM/2.14 SSL-MM/1.4.1 OpenSSL/1.0.0a").should eql("oth-otd") }
+    specify { subject.browser_and_platform_key("Mozilla/5.0 (X11; U; Linux armv7l; ru-RU; rv:1.9.2.3pre) Gecko/20100723 Firefox/3.5 Maemo Browser 1.7.4.8 RX-51 N900").should eql("fir-lin") }
+    specify { subject.browser_and_platform_key("Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (J2ME/23.377; U; en) Presto/2.5.25 Version/10.54").should eql("oth-otm") }
+    specify { subject.browser_and_platform_key("Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3").should eql("saf-iph") }
+    specify { subject.browser_and_platform_key("Mozilla/5.0(iPad; U; CPU OS 4_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8F191 Safari/6533.18.5").should eql("saf-ipa") }
+    specify { subject.browser_and_platform_key("Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10").should eql("saf-ipa") }
+    specify { subject.browser_and_platform_key("Mozila/5.0 (iPod; U; CPU like Mac OS X; en) AppleWebKit/420.1 (KHTML, like Geckto) Version/3.0 Mobile/3A101a Safari/419.3").should eql("saf-ipo") }
+    specify { subject.browser_and_platform_key("HotJava/1.1.2 FCS").should eql("oth-otd") }
+    specify { subject.browser_and_platform_key("").should eql("oth-otd") }
+  end
 
 end
