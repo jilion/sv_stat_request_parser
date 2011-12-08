078379974269953af31c6d221a7121d28f4143e2
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2011-12-08 12:42:52 +0100

    Add convert_incs_to_json

--- lib/stat_request_parser.rb
+++ lib/stat_request_parser.rb
@@ -88,6 +88,54 @@ module StatRequestParser
     raise BadParamsError
   end
 
+  # Convert StatRequestParser incs to json for Pusher
+  #
+  # { site: { id: ...}, videos: [ { id: ... u: } ]}
+  #
+  def self.convert_incs_to_json(incs, second)
+    json = { site: {}, videos: [] }
+    site = incs[:site]
+    if site[:inc].present?
+      json[:site] = { id: second }
+      site[:inc].each do |key, value|
+        case key
+        when /^pv\./
+          json[:site][:pv] = value
+        when /^bp\./
+          one_level_json_convert!(json[:site], key, value)
+        when /^md\./
+          double_level_json_convert!(json[:site], key, value)
+        when /^vv\./
+          json[:site][:vv] = value
+        end
+      end
+    end
+    incs[:videos].each do |video|
+      if video[:inc].present?
+        video_json = { id: second, u: video[:u] }
+        video_json[:n] = video[:n] if video.key?(:n)
+        video[:inc].each do |key, value|
+          case key
+          when /^vl\./
+            video_json[:vl] = value
+          when /^bp\./
+            one_level_json_convert!(video_json, key, value)
+          when /^md\./
+            double_level_json_convert!(video_json, key, value)
+          when /^vs\./
+            one_level_json_convert!(video_json, key, value)
+          when /^vv\./
+            video_json[:vv] = value
+          end
+        end
+        json[:videos] << video_json
+      end
+    end
+    json
+  end
+
+private
+
   def self.browser_and_platform_key(user_agent)
     useragent    = UserAgent.parse(user_agent)
     browser_key  = SUPPORTED_BROWSER[useragent.browser] || "oth"
@@ -117,5 +165,18 @@ module StatRequestParser
     "webOS"         => "weo",
     "Windows Phone" => "wip"
   }
+
+
+  def self.one_level_json_convert!(json, key, value)
+    keys = key.split('.')
+    json[keys[0].to_sym] = { keys[1] => value }
+  end
+
+  def self.double_level_json_convert!(json, key, value)
+    keys = key.split('.')
+    json[keys[0].to_sym] ||= {}
+    json[keys[0].to_sym][keys[1]] ||= {}
+    json[keys[0].to_sym][keys[1]].merge!(keys[2] => value)
+  end
 
 end
--- spec/stat_request_parser_spec.rb
+++ spec/stat_request_parser_spec.rb
@@ -254,4 +254,55 @@ describe StatRequestParser do
     specify { subject.browser_and_platform_key("").should eql("oth-otd") }
   end
 
+  describe ".convert_incs_to_json" do
+    let(:second) { 12345678 }
+    before(:each) { @json = subject.convert_incs_to_json(incs, second) }
+
+    context "load event with 2 video loaded" do
+      let(:incs) { {
+        site: { t: 'site1234', inc: { "pv.m" => 1, "bp.saf-osx" => 1, "md.h.d" => 1, "md.f.d" => 1 } },
+        videos: [
+          { st: 'site1234', u: 'abcd1234', inc: { "vl.m" => 1, "bp.saf-osx" => 1, "md.h.d" => 1 } },
+          { st: 'site1234', u: 'efgh5678', inc: { "vl.m" => 1, "bp.saf-osx" => 1, "md.f.d" => 1 } }
+        ]
+      } }
+
+      specify { @json.should eql(
+        site: { id: 12345678, pv: 1, bp: { 'saf-osx' => 1 }, md: { 'h' => { 'd' => 1 }, 'f' => { 'd' => 1 } } },
+        videos: [
+          { id: 12345678, u: 'abcd1234', vl: 1, bp: { 'saf-osx' => 1 }, md: { 'h' => { 'd' => 1 } } },
+          { id: 12345678, u: 'efgh5678', vl: 1, bp: { 'saf-osx' => 1 }, md: { 'f' => { 'd' => 1 } } }
+        ]
+      ) }
+    end
+
+    context "load event with empty video loaded" do
+      let(:incs) { {
+        site: { t: 'site1234', inc: { "pv.e" => 1, "bp.saf-osx" => 1, "md.h.d" => 1 } },
+        videos: []
+      } }
+
+      specify { @json.should eql(
+        site: { id: 12345678, pv: 1, bp: { 'saf-osx' => 1 }, md: { 'h' => { 'd' => 1 } } },
+        videos: []
+      ) }
+    end
+
+    context "view event" do
+      let(:incs) { {
+        site: { t: 'site1234', inc: { "vv.m" => 1 } },
+        videos: [
+          { st: 'site1234', u: 'abcd1234', n: 'My Video', inc: { "vv.m" => 1, "vs.source12" => 1 } }
+        ]
+      } }
+
+      specify { @json.should eql(
+        site: { id: 12345678, vv: 1 },
+        videos: [
+          { id: 12345678, u: 'abcd1234', n: 'My Video', vv: 1, vs: { 'source12' => 1 } },
+        ]
+      ) }
+    end
+  end
+
 end
